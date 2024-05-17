a1ed38de62ba02e039fe3dc83206c8eb548749a8
Carlosgrpl <cgonzalez.38@alumni.unav.es>
2024-05-17 22:25:46 +0200

    First try at Angular, lets go!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstHeaderComponent } from './components/first-header/first-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FirstHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularProject';
+  username: string = "Carlos";
 }
